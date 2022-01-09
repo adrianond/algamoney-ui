eda777b05627bd8e4c9297cc81eca597b9dc68e8
adriano.dantas <adriano.dantas@omni.com.br>
2022-01-08 21:14:48 -0300

    Adicionando o formulário de pesquisa de lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {TabViewModule} from 'primeng/tabview';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import {TooltipModule} from 'primeng/tooltip';
 
 @NgModule({
   declarations: [
@@ -11,7 +14,10 @@ import {TabViewModule} from 'primeng/tabview';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TabViewModule
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
