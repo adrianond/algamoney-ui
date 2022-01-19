26691422215352de751cf093fd6123ecb4f0a5fb
adriano.dantas <adriano.dantas@omni.com.br>
2022-01-19 09:37:19 -0300

    criação modulo de lançamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
+import { LancamentoModule } from './lancamento/lancamento.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/pessoas-pesquisa.component';
+/* import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button'; */
 import { NavbarComponent } from './navbar/navbar.component';
 import { LOCALE_ID } from '@angular/core';
 import ptBr from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
-import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
-import { DropdownModule } from 'primeng/dropdown';
+/* import { DropdownModule } from 'primeng/dropdown';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { CalendarModule } from 'primeng/calendar';
 import { SelectButtonModule } from 'primeng/selectbutton';
@@ -19,53 +18,43 @@ import { FormsModule } from '@angular/forms';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
 import { InputNumberModule } from 'primeng/inputnumber';
-import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
-import { InputMaskModule } from 'primeng/inputmask';
+import { InputMaskModule } from 'primeng/inputmask'; */
 import { AppMessageComponent } from './app-message/app-message.component';
-import { MessageModule } from 'primeng/message';
-import { CurrencyMaskModule } from "ng2-currency-mask";
-import { PesquisaPessoasComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-import { NgxMaskModule, IConfig } from 'ngx-mask';
-import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.component';
-import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component'
-
+/* import { MessageModule } from 'primeng/message'; */
+/* import { CurrencyMaskModule } from 'ng2-currency-mask'; */
+/* import { NgxMaskModule, IConfig } from 'ngx-mask'; */
+import { PessoaModule } from './pessoa/pessoa.module';
 
 registerLocaleData(ptBr);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LancamentosPesquisaComponent,
-    NavbarComponent,
-    PesquisaPessoasComponent,
-    LancamentoCadastroComponent,
-    PessoaCadastroComponent,
-    AppMessageComponent,
-    LancamentosGridComponent,
-    PessoasGridComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    InputTextModule,
-    InputNumberModule,
-    ButtonModule,
-    TableModule,
-    TooltipModule,
-    DropdownModule,
-    InputTextareaModule,
-    CalendarModule,
-    SelectButtonModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    InputMaskModule,
-    MessageModule,
-    CurrencyMaskModule,
-    NgxMaskModule.forRoot() 
-  ],
-  providers: [
-    { provide: LOCALE_ID, useValue: 'pt' },
-  ],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NavbarComponent,
+		AppMessageComponent
+],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+	/* 	InputTextModule,
+		InputNumberModule,
+		ButtonModule,
+		TableModule,
+		TooltipModule,
+		DropdownModule,
+		InputTextareaModule,
+		CalendarModule,
+		SelectButtonModule,
+		BrowserAnimationsModule,
+		FormsModule,
+		InputMaskModule, */
+		/* MessageModule, */
+		/* CurrencyMaskModule, */
+		PessoaModule,
+		LancamentoModule,
+		/* NgxMaskModule.forRoot() */
+	],
+	providers: [ { provide: LOCALE_ID, useValue: 'pt' } ],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
